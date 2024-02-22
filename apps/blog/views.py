be2ba86059987cd11c3fabd16d001a72f6073256
BadViFi <rudijoleg7@gmail.com>
2024-02-22 18:21:43 +0200

    add_likes_and_coments

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 
-from .models import Post
+from .models import Post,Comment
 
 from .forms import PostForm,CommentForm
 
@@ -67,3 +67,12 @@ def like(request, post_id):
     post.likes += 1
     post.save()
     return JsonResponse({'likes': post.likes})
+
+
+
+
+def like_comment(request, post_id, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment.likes += 1
+    comment.save()
+    return JsonResponse({'likes': comment.likes})
