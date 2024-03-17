61881b1b89c99cd7a34fa94ab31d479e1bfdbbd6
BadViFi <rudijoleg7@gmail.com>
2024-03-17 18:25:28 +0200

    end_search_start_debug

--- apps/members/urls.py
+++ apps/members/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 from .views import *
 
-
 app_name = 'members'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup'),
+    path('profile/<str:username>', profile_view, name='profile'),
     path('profile/', profile_view, name='profile'),
     path('profile/update/', profile_update_view, name='profile_update'),
-]
+    path('profile/<str:username>/follow/', follow_view, name='follow'),
+    
+    path('search/', search_view, name='search_profile'),
+    
+    ] 
