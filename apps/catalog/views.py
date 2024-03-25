cdc9119ae723c98dfc227b8b52ead927890ca124
BadViFi <rudijoleg7@gmail.com>
2024-03-25 11:18:41 +0200

    add_amount_of_catalog_and_products

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -11,7 +11,15 @@ class CataloglistView(ListView):
 
     def get_queryset(self):
         return Catalog.objects.filter(parent=None)
+            
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_count'] = Catalog.objects.filter(parent=None).count()
+        print(context)
+        return context
+    
 
+    
 
 class ProductByCategoryView(ListView):
     model = Catalog
@@ -22,9 +30,7 @@ class ProductByCategoryView(ListView):
         self.category = Catalog.objects.get(slug=self.kwargs['slug'])
         self.categories = Catalog.objects.filter(parent=self.category)
         self.all_categories = self.categories.get_descendants(include_self=True)
-        print(self.all_categories)
         queryset = Product.objects.filter(productcategory__category__in=self.all_categories)
-        print(queryset)
         return queryset
 
     def get_context_data(self, **kwargs):
@@ -32,7 +38,13 @@ class ProductByCategoryView(ListView):
         context['categories'] = self.categories
         context['category'] = self.category
         return context
-
+    
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_queryset = len(list(context['category']))
+        context["amount_of_products"] = product_queryset
+        return context
 
 class ProductDetailView(DetailView):
     model = Product
